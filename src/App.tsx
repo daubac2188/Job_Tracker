33abf16537ff3849cec2c55f50e5ec516e5336de
Nguyen Andy <>
2023-02-07 09:43:04 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Login from './pages/Login'
+import Home from './pages/Home'
+export default function App() {
+
+
+  return (
+    <main>
+
+      <Home />
+    </main>
+  )
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import Container from '@mui/material/Container'
+
+const Home = () => {
+
+  return (
+    <Container maxWidth='lg'>
+      Homeadasd
+    </Container>
+  )
+}
+
+export default Home
